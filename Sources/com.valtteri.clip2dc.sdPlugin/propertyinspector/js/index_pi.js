5624543e572abed98baed61e10ce5792176d8019
Valtteri Lappalainen <valtteri.lappalainen@tuni.fi>
2021-10-11 23:22:31 +0300

    Basic functionality for index_pi

--- Sources/com.valtteri.clip2dc.sdPlugin/propertyinspector/js/index_pi.js
+++ Sources/com.valtteri.clip2dc.sdPlugin/propertyinspector/js/index_pi.js
@@ -1,31 +1,20 @@
 $SD.on('connected', (jsn) => {
-    /**
-     * The passed 'applicationInfo' object contains various information about your
-     * computer, Stream Deck version and OS-settings (e.g. colors as set in your
-     * OSes display preferences.)
-     * We use this to inject some dynamic CSS values (saved in 'common_pi.js'), to allow
-     * drawing proper highlight-colors or progressbars.
-     */
-
     console.log("connected");
-    addDynamicStyles($SD.applicationInfo.colors, 'connectSocket');
-
-    /**
-     * Current settings are passed in the JSON node
-     * {actionInfo: {
-     *      payload: {
-     *          settings: {
-     *                  yoursetting: yourvalue,
-     *                  otherthings: othervalues
-     * ...
-     * To conveniently read those settings, we have a little utility to read
-     * arbitrary values from a JSON object, eg:
-     *
-     * const foundObject = Utils.getProp(JSON-OBJECT, 'path.to.target', defaultValueIfNotFound)
-     */
-
-    settings = Utils.getProp(jsn, 'actionInfo.payload.settings', false);
-    if (settings) {
-        updateUI(settings);
+    console.log(jsn);
+    if(jsn.hasOwnProperty("actionInfo")) {
+        let settings = Utils.getProp(jsn, "actionInfo.payload.settings", {});
+        document.getElementById("channelName").value = settings.channelName || ""
+        document.getElementById("twitchToken").value = settings.twitchToken || ""
+        document.getElementById("dcWebhook").value = settings.dcWebhook || ""
     }
 });
+
+const save = function() {
+    if($SD) {
+        var payload = {};
+        [].forEach.call(document.querySelectorAll(".inspector"), element => {
+            payload[element.id] = element.value;
+        });
+        $SD.api.sendToPlugin($SD.uuid, $SD.actionInfo["action"], payload);
+    }
+}
