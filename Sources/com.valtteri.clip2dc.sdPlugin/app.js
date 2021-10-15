23e3eb716ba19f5efa7592313e0f3e98edb817c3
Valtteri Lappalainen <valtteri.lappalainen@tuni.fi>
2021-10-15 03:52:36 +0300

    Made action images and renamed plugin

--- Sources/com.valtteri.clip2dc.sdPlugin/app.js
+++ Sources/com.valtteri.clip2dc.sdPlugin/app.js
@@ -2,14 +2,14 @@ $SD.on('connected', (jsonObj) => connected(jsonObj));
 
 function connected(jsn) {
     // Subscribe to the willAppear and other events
-    $SD.on('com.valtteri.clip2dc.action.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
-    $SD.on('com.valtteri.clip2dc.action.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
-    $SD.on('com.valtteri.clip2dc.action.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
-    $SD.on('com.valtteri.clip2dc.action.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
-    $SD.on('com.valtteri.clip2dc.action.propertyInspectorDidAppear', (jsonObj) => {
+    $SD.on('com.valtteri.clip2dc.action.clip.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
+    $SD.on('com.valtteri.clip2dc.action.clip.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
+    $SD.on('com.valtteri.clip2dc.action.clip.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
+    $SD.on('com.valtteri.clip2dc.action.clip.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
+    $SD.on('com.valtteri.clip2dc.action.clip.propertyInspectorDidAppear', (jsonObj) => {
         console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js]propertyInspectorDidAppear:');
     });
-    $SD.on('com.valtteri.clip2dc.action.propertyInspectorDidDisappear', (jsonObj) => {
+    $SD.on('com.valtteri.clip2dc.action.clip.propertyInspectorDidDisappear', (jsonObj) => {
         console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js]propertyInspectorDidDisappear:');
     });
 };
