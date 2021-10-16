c2e219e332f0a32ee6fd27ded0247728bd96716e
Valtteri Lappalainen <valtteri.lappalainen@tuni.fi>
2021-10-16 18:57:38 +0300

    Fixed saving editClip info

--- Sources/com.valtteri.clip2dc.sdPlugin/propertyinspector/js/index_pi.js
+++ Sources/com.valtteri.clip2dc.sdPlugin/propertyinspector/js/index_pi.js
@@ -5,7 +5,18 @@ $SD.on('connected', (jsn) => {
         let settings = Utils.getProp(jsn, "actionInfo.payload.settings", {});
         Object.keys(settings).forEach(key => {
             try {
-                document.getElementById(key).value = settings[key] || "";
+                if (key === "editClip") {
+                    if (settings[key]) {
+                        document.getElementById("editClip").checked = true;
+                        document.getElementById("notEditClip").checked = false;
+                    }
+                    else {
+                        document.getElementById("editClip").checked = false;
+                        document.getElementById("notEditClip").checked = true;
+                    }
+                } else {
+                    document.getElementById(key).value = settings[key] || "";
+                }
             } catch (error) {
                 console.error(error);
             }
@@ -29,7 +40,8 @@ const save = function() {
         [].forEach.call(document.querySelectorAll(".inspector"), element => {
             payload[element.id] = element.value;
         });
-        payload["editClip"] = document.getElementById("rdio1").checked
+        payload["editClip"] = document.getElementById("editClip").checked
+        console.log(payload);
         $SD.api.sendToPlugin($SD.uuid, $SD.actionInfo["action"], payload);
         var globalPayload = {};
         [].forEach.call(document.querySelectorAll(".globalsetting"), element => {
